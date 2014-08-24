9497a40be24da7965e906ff7000095043cc3383a
ValeryCherepanov <valery.cherepanov@gmail.com>
2014-08-24 20:39:48 +0400

    zzzz

--- src/com/vcherepanov/cassandra/ShowKeyspaces.java
+++ src/com/vcherepanov/cassandra/ShowKeyspaces.java
@@ -8,7 +8,7 @@ public class ShowKeyspaces {
 
     public static void main(String[] args) throws Exception {
 
-        String host = "127.0.0.2сссZZ";
+        String host = "127.0.0.2сссZZ111";
         int port = 9160;
         TSocket socket = new TSocket(host, port);
         TTransport transport = new TFramedTransport(socket);
